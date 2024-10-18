c181a35ef653da6bcd9c46efd9441eec3335a611
Lance Bachmeier <lanceb@ksu.edu>
2024-10-18 10:52:44 -0500

    Integrating rng with betterr

--- gsl/rng/random.d
+++ gsl/rng/random.d
@@ -12,10 +12,14 @@
  * Since this uses ImportC, you need to add a command like -P-Igsl/rng gsl/rng/*.c
  * when compiling. That tells the D compiler where to find the header
  * and source files.
+ * 
+ * This file relies on betterr. Call the C files directly if you don't
+ * want that - this is part of betterr, after all.
  */
 module gsl.rng.random;
 import distributions, gslrng, mt;
 import core.stdc.config;
+import betterr.vector;
 
 alias RNGType = gsl_rng_type*;
 alias RNG = gsl_rng*;
@@ -28,6 +32,19 @@ RNG allocRNG(RNGType type) { return gsl_rng_alloc(type); }
 RNG allocRNG() { return gsl_rng_alloc(gsl_rng_mt19937); }
 void freeRNG(RNG r) { gsl_rng_free(r); }
 void setSeed(RNG r, c_ulong seed) { gsl_rng_set(r, seed); }
+
+Vector rnorm(long n, RNG r, double mu=0.0, double sd=1.0) {
+  auto result = Vector(n);
+  foreach(ii; 0..n) {
+    result[ii] = rnorm(r, mu, sd);
+  }
+  return result;
+}
+
+// This is the faster of the methods.
+double rnorm(RNG r, double mu=0.0, double sd=1.0) {
+  return gsl_ran_gaussian_ratio_method(r, sd) + mu;
+}
 
 /* Using the R naming convention of `r` at the start of functions that
  * generate random draws. Same parameters as GSL unless otherwise noted.
